01c0c59d5945159d7bebdf34fd192dadac4c11f4
rildodev <rildombs@gmail.com>
2021-01-24 18:52:25 -0300

    criando os métodos construtores da super classe

--- src/entidades/Contribuinte.java
+++ src/entidades/Contribuinte.java
@@ -5,6 +5,18 @@ public class Contribuinte {
 	private String nome;
 	private Double rendaAnual;
 	
+	public Contribuinte() {
+		super();
+	}
+
+	public Contribuinte(String nome, Double rendaAnual) {
+		super();
+		this.nome = nome;
+		this.rendaAnual = rendaAnual;
+	}
+	
+	
+	
 	
 	
 	
